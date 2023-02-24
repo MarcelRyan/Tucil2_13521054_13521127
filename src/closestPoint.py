189e7986369e2c5334ba724b6d45554c19eae84e
MarcelRyan <marcel.ryan2004@gmail.com>
2023-02-24 23:07:51 +0700

    divide and conquer done but not final?

--- src/closestPoint.py
+++ src/closestPoint.py
@@ -2,6 +2,7 @@ import random
 import math
 import numpy as np
 import sys
+import time
 
 def inputUser():
     n = int(input("Masukkan berapa titik yang ingin di generate: "))
@@ -10,8 +11,10 @@ def inputUser():
     for i in range(n):
         points = []
         for j in range(dimensi):
-            points.append(random.uniform(0, 10))
+            points.append(random.uniform(0, 100))
         array.append(points)
+
+    # Mengurutkan array dari x axis menaik
     for i in range(n):
         min_idx = i
         for j in range(min_idx+1, n):
@@ -23,6 +26,8 @@ def inputUser():
     return array, dimensi
 
 def minDistanceBruteForce(array, dimensi):
+    count = 0
+    start = time.time()
     array_distance = []
     for i in range(len(array)):
         temp = []
@@ -31,6 +36,7 @@ def minDistanceBruteForce(array, dimensi):
             for k in range(dimensi):
                 jarak += (array[j][k] - array[i][k]) ** 2
             temp.append(math.sqrt(jarak))
+            count += 1
         array_distance.append(temp)
     min = array_distance[0][0]
     point1= 0
@@ -41,7 +47,10 @@ def minDistanceBruteForce(array, dimensi):
                 min = array_distance[i][j]
                 point1 = i
                 point2 = j+i+1
+    end = time.time()
     print(f"Titik terdekat dengan algoritma bruteforce adalah titik {point1} dan titik {point2} dengan jarak sebesar {min}")
+    print(f"Waktu yang diperlukan untuk algoritma bruteforce adalah {end-start}")
+    print(f"Jumlah operasi euclidean distance algoritma brute force adalah {count}")
     return point1, point2
 
 def distance(point1, point2, dimensi):
@@ -50,12 +59,31 @@ def distance(point1, point2, dimensi):
         jarak += (point2[i] - point1[i]) ** 2
     return math.sqrt(jarak)
 
+count = 0
 def divideAndConquer(points, dimensi):
+    global count
     array = []
     if (len(points) == 2):
         array = points
         min = distance(points[0], points[1], dimensi)
+        count += 1
         return min, array
+    elif (len(points) == 3):
+        min1 = distance(points[0], points[1], dimensi)
+        min2 = distance(points[0], points[2], dimensi)
+        min3 = distance(points[1], points[2], dimensi)
+        if (min1 <= min2 and min1 <= min3):
+            array = [points[0], points[1]]
+            count += 3
+            return min1, array
+        elif (min2 <= min1 and min2 <= min3):
+            array = [points[0], points[2]]
+            count += 3
+            return min2, array
+        else:
+            array = [points[1], points[2]]
+            count += 3
+            return min3, array
     else:
         mid = len(points)//2
         left = []
@@ -78,11 +106,23 @@ def divideAndConquer(points, dimensi):
 
         midPoint = []
         # Mengambil titik yang jarak x nya lebih kecil atau sama dengan min dari titik x median
-        for i in range(len(points)):
-            if (abs(points[i][0] - points[mid][0]) <= min):
-                midPoint.append(points[i])
+        for i in range(len(left)):
+            bisa = True
+            for k in range(dimensi):
+                if (abs(left[i][k] - points[mid][k]) > min):
+                    bisa = False
+            if bisa:
+                midPoint.append(left[i])
         
+        for i in range(len(right)):
+            bisa = True
+            for k in range(dimensi):
+                if (abs(right[i][k] - points[mid][k]) > min):
+                    bisa = False
+            if bisa:
+                midPoint.append(right[i])
         # Mengurutkan titik berdasarkan y menurun
+
         for i in range(len(midPoint)):
             min_idx = i
             for j in range(min_idx+1, len(midPoint)):
@@ -94,17 +134,15 @@ def divideAndConquer(points, dimensi):
         
         for i in range(len(midPoint)):
             for j in range(i+1, len(midPoint)):
-                proses = True
-                for k in range(dimensi):
-                    if (abs(midPoint[i][k] - midPoint[j][k]) > min):
-                        proses = False
-                        break
-                if (proses):
-                    minMid = distance(midPoint[i], midPoint[j], dimensi)
-                    if (minMid < min):
-                        min = minMid
-                        array = [midPoint[i], midPoint[j]]
+                minMid = distance(midPoint[i], midPoint[j], dimensi)
+                count += 1
+                if (minMid < min):
+                    min = minMid
+                    array = [midPoint[i], midPoint[j]]
         return min, array
+
+def jumlahEucDNC():
+    print("Jumlah operasi euclidean untuk algoritma divide and conquer adalah", count)
 
 def indexDNC(array, points):
 
--- src/visualization.py
+++ src/visualization.py
@@ -3,23 +3,33 @@ from mpl_toolkits.mplot3d import Axes3D
 import random
 import math
 import closestPoint
+import time
 
 fig = plt.figure(figsize=(10,10))
 ax = fig.add_subplot(111, projection='3d')
 array_of_points, dimensi = closestPoint.inputUser()
 point1, point2 = closestPoint.minDistanceBruteForce(array_of_points, dimensi)
-mindnc, arraydnc = closestPoint.divideAndConquer(array_of_points, dimensi)
+start = time.time()
+mindnc, arraydnc  = closestPoint.divideAndConquer(array_of_points, dimensi)
+end = time.time()
 idx1, idx2 = closestPoint.indexDNC(arraydnc, array_of_points)
 print(f"Titik terdekat dengan algoritma divide and conquer {idx1} dan titik {idx2} dengan jarak sebesar {mindnc}")
+print(f"waktu yang dibutuhkan algoritma divide and conquer adalah {end-start}")
+closestPoint.jumlahEucDNC()
+# print(f"Jumlah operasi euclidean distance algoritma divide and conquer adalah {count}")
 
 if (dimensi == 3):
     for i in range(len(array_of_points)):
         if (i == idx1 or i == idx2):
             ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "red")
         else:
-            ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "blue")
+            ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], c= 'b')
 
 
     plt.plot([array_of_points[idx1][0] ,array_of_points[idx2][0]] , [array_of_points[idx1][1], array_of_points[idx2][1]], [array_of_points[idx1][2], array_of_points[idx2][2]], color = 'black',  linestyle = '-')
     plt.ioff()
+    plt.title("3D Scatter Plot")
+    ax.set_xlabel("Sumbu X")
+    ax.set_ylabel("Sumbu Y")
+    ax.set_zlabel("Sumbu Z")
     plt.show()
