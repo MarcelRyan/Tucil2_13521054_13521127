6fafd180032e9556ff4227bad62d6d37cc7d47d3
MarcelRyan <marcel.ryan2004@gmail.com>
2023-02-24 12:11:53 +0700

    add restriction for visualizer and brute force

--- src/closestPoint.py
+++ src/closestPoint.py
@@ -1,22 +1,23 @@
 import random
 import math
 
-def inputDimensi():
+def inputUser():
     n = int(input("Masukkan berapa titik yang ingin di generate: "))
     dimensi = int(input("Ingin berapa dimensi: "))
     array = []
     for i in range(n):
-        x_axes = random.randint(0, 10)
-        y_axes = random.randint(0, 10)
-        z_axes = random.randint(0, 10)
-        array.append([x_axes, y_axes, z_axes])
-    return array
+        points = []
+        for j in range(dimensi):
+            points.append(random.randint(0, 10))
+        array.append(points)
+    return array, dimensi
 
-def minDistanceBruteForce(array):
+def minDistanceBruteForce(array, dimensi):
     array_distance = []
     for i in range(0, len(array), 2):
         jarak = 0
-        jarak += (array[i+1][0] - array[i][0]) ** 2 + (array[i+1][1] - array[i][1]) ** 2 + (array[i+1][2] - array[i][2]) ** 2
+        for j in range(dimensi):
+            jarak += (array[i+1][j] - array[i][j]) **2
         print(math.sqrt(jarak))
         array_distance.append(math.sqrt(jarak))
     
@@ -24,6 +25,7 @@ def minDistanceBruteForce(array):
     idx_min = 0
     for i in range(len(array_distance)):
         if (min > array_distance[i]):
+            min = array_distance[i]
             idx_min = i
             print(idx_min)
     return idx_min
--- src/visualization.py
+++ src/visualization.py
@@ -6,16 +6,17 @@ import closestPoint
 
 fig = plt.figure(figsize=(10,10))
 ax = fig.add_subplot(111, projection='3d')
-array_of_points = closestPoint.input3D()
-idx_min = closestPoint.minDistanceBruteForce(array_of_points)
+array_of_points, dimensi = closestPoint.inputUser()
+idx_min = closestPoint.minDistanceBruteForce(array_of_points, dimensi)
 
-for i in range(len(array_of_points)):
-    if (i == idx_min * 2 or i == idx_min * 2 + 1):
-        ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "red")
-    else:
-        ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "blue")
+if (dimensi == 3):
+    for i in range(len(array_of_points)):
+        if (i == idx_min * 2 or i == idx_min * 2 + 1):
+            ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "red")
+        else:
+            ax.scatter(array_of_points[i][0], array_of_points[i][1], array_of_points[i][2], color = "blue")
 
 
-plt.plot([array_of_points[idx_min*2][0] ,array_of_points[idx_min*2+1][0]] , [array_of_points[idx_min*2][1], array_of_points[idx_min*2+1][1]], [array_of_points[idx_min*2][2], array_of_points[idx_min*2+1][2]], color = 'black',  linestyle = '-')
-plt.ioff()
-plt.show()
+    plt.plot([array_of_points[idx_min*2][0] ,array_of_points[idx_min*2+1][0]] , [array_of_points[idx_min*2][1], array_of_points[idx_min*2+1][1]], [array_of_points[idx_min*2][2], array_of_points[idx_min*2+1][2]], color = 'black',  linestyle = '-')
+    plt.ioff()
+    plt.show()
